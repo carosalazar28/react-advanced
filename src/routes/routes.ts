9cf66dc25345ba646e4a8766b2655be17aaed009
Carolina <cindy.s.r.28@hotmail.com>
2022-04-16 16:40:56 -0500

    initial configuration with routes simples

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,46 +1,57 @@
-import { lazy, LazyExoticComponent } from "react";
-import { ProfileNoLazy } from "../lazyload/pages/ProfileNoLazy";
+// import { lazy, LazyExoticComponent } from "react";
+// import { ProfileNoLazy } from "../lazyload/pages/ProfileNoLazy";
+import { Home, About, Users } from "../lazyload/pages/Index";
 
 type JSXComponent = () => JSX.Element;
 
+/*
 interface Route {
   to: string;
   path: string;
   Component: LazyExoticComponent<() => JSX.Element> | JSXComponent;
   name: string;
 }
+*/ // Interface deinition for a route that is not lazy loaded and lazy loaded
 
-// Lazyload definition with personalize chunk names
+interface Route {
+  to: string;
+  path: string;
+  Component: JSXComponent;
+  name: string;
+}
+
+/*Lazyload definition with personalize chunk names
 const LazyLayoutHome = lazy(
   () => import("../lazyload/components/layout/LazyLayout")
-);
+);*/ // Lazyload definition for a module that hace nested routes
+
 // const LazyAbout = lazy(() => import("../lazyload/pages/About"));
 // const LazyUsers = lazy(() => import("../lazyload/pages/Users"));
 
 export const routes: Route[] = [
+  // {
+  //   to: "/lazyload",
+  //   // For react router dom v6 this is the way for render children routes
+  //   path: "/lazyload/*",
+  //   Component: LazyLayoutHome,
+  //   name: "LazyLoad",
+  // },
   {
-    to: "/lazyload",
-    // For react router dom v6 this is the way for render children routes
-    path: "/lazyload/*",
-    Component: LazyLayoutHome,
-    name: "LazyLoad",
+    to: "/",
+    path: "/",
+    Component: Home,
+    name: "Home",
   },
-  // {
-  //   to: "/about",
-  //   path: "about",
-  //   Component: LazyAbout,
-  //   name: "About",
-  // },
-  // {
-  //   to: "/users",
-  //   path: "users",
-  //   Component: LazyUsers,
-  //   name: "Users",
-  // },
   {
-    to: "/profile-no-lazy",
-    path: "profile-no-lazy",
-    Component: ProfileNoLazy,
-    name: "Profile No Lazy",
+    to: "/about",
+    path: "about",
+    Component: About,
+    name: "About",
+  },
+  {
+    to: "/users",
+    path: "users",
+    Component: Users,
+    name: "Users",
   },
 ];
